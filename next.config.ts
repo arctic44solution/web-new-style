d4f88a3a909e91af2587c668093be262b275e20a
arctic44com <contact@arctic44.com>
2026-05-15 13:24:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export', // Forces Next.js to generate static HTML/CSS/JS
+  images: {
+    unoptimized: true,
+  };
 
 export default nextConfig;
